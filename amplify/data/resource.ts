05256056ac0fc0387034cac75bf018cfc7615d15
midhunxavier <midhunxavier@outlook.com>
2025-02-18 22:33:19 +0100

    apiKey addition

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,6 +11,11 @@ const schema = a.schema({
 });
 
 export type Schema = ClientSchema<typeof schema>;
+
 export const data = defineData({
   schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: { expiresInDays: 30 },
+  },
 });
